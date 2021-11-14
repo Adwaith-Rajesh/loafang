ba187920998914782968804bd5bbc521be009b39
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-11-14 15:44:20 +0530

    Addded dataclasses for Execution block and the query string and it's contents

--- loafang/_dataclasses.py
+++ loafang/_dataclasses.py
@@ -1,5 +1,11 @@
+from argparse import Namespace
 from dataclasses import dataclass
+from dataclasses import field
+from typing import Any
+from typing import Dict
+from typing import List
 from typing import Optional
+from typing import Union
 
 
 @dataclass
@@ -7,3 +13,23 @@ class Header:
     method: str
     id: str
     property_key: Optional[str] = None
+
+
+@dataclass
+class ListQuery:
+    # used when the method is get, delete
+    args: Namespace
+    contents: List[str] = field(default_factory=list)
+
+
+@dataclass
+class DictQuery:
+    # used when the values passed for methods are key value pairs
+    args: Namespace
+    contents: Dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class ExecutionBlock:
+    header: Header
+    query: List[Union[ListQuery, DictQuery]] = field(default_factory=list)
